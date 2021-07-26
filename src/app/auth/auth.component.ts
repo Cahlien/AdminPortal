0242d6e2115549988fbf00733ff0f1ad81743b2f
Matthew Crowell <>
2021-07-26 18:20:44 -0500

    implement persistence for token and user id

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  error: string = '';
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm) {
+    if(!form.valid){
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    let response = this.authService.login(email, password)
+      .subscribe((response: any) => {
+        const token = response.headers.get('Authorization');
+        const userId = response.headers.get('BTUID');
+        localStorage.setItem("token", token);
+        localStorage.setItem("userId", userId);
+      })
+  }
 }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+  url: string = 'http://localhost:9001/users/login';
+  headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json', 'Accept': 'application/json'});
+
+  constructor(private http: HttpClient) {
+  }
+
+  login(email: string, password: string) {
+    return this.http.post(
+      this.url,
+      {
+        email,
+        password
+      },
+      {
+        observe: "response",
+        headers: this.headers
+      });
+  }
+}
